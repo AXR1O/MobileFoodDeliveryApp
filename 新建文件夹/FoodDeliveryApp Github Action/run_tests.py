c93da0ed91f71e57a86ed5b03d17972a0158304d
AXRIO <2996034962@qq.com>
2024-12-16 10:58:49 +0800

    Delete 新建文件夹/FoodDeliveryApp Github Action directory

--- 新建文件夹/FoodDeliveryApp Github Action/run_tests.py
+++
@@ -1,6 +0,0 @@
-import unittest
-if __name__ == '__main__':
-    loader = unittest.TestLoader()
-    tests = loader.discover('tests')
-    testRunner = unittest.runner.TextTestRunner()
-    testRunner.run(tests)
